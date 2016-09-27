295ea6df68d5175e98f578295499888c69406dd7
Paride Balestri IPREL <paride_balestri@iprel.it>
2016-09-27 15:56:37 +0200

    2016.09.27 - Versione 0

---
+++ AfterSalesDBEditor/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace AfterSalesDBEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(580, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(42, 96);
+            this.textBox1.MaxLength = 4;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 43);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.WordWrap = false;
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(374, 96);
+            this.textBox2.MaxLength = 4;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 43);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.WordWrap = false;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(38, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Catalogo di partenza";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(359, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nuovo nome catalogo";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Image = global::AfterSalesDBEditor.Properties.Resources.arrow_17_64;
+            this.button1.Location = new System.Drawing.Point(259, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 70);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(480, 89);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(148, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(580, 213);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "AfterSales DB Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AfterSalesDBEditor/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+
+namespace AfterSalesDBEditor
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+            button1.Visible = false;
+
+
+
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (CataloghiEntities listaCataloghi = new CataloghiEntities())
+            {
+                string header = textBox1.Text.Substring(0, 1);
+                int number;
+                int.TryParse(textBox1.Text.Substring(1, 3), out number);
+
+                string headerNew = textBox2.Text.Substring(0, 1);
+                int numberNew;
+                int.TryParse(textBox2.Text.Substring(1, 3), out numberNew);
+
+                var cataQuery1 = from Cataloghi in listaCataloghi.Cataloghis where Cataloghi.PrefissoNome == header && Cataloghi.SuffissoNome == number orderby Cataloghi.SuffissoNome select Cataloghi;
+
+                foreach (var catal in cataQuery1)
+                {
+                    catal.PrefissoNome = headerNew;
+                    catal.SuffissoNome = numberNew;
+                }
+
+                try
+                {
+                    listaCataloghi.SaveChanges();
+                    MessageBox.Show("Variazione eseguita!", "Esito...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                    pictureBox1.Image = null;
+                    pictureBox2.Image = null;
+                    textBox1.Focus();
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                listaCataloghi.Dispose();
+            }
+
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            //lasciando il controllo vado a verificare nel DB quanto richiesto
+
+            if (textBox1.Text.Length == 4 && char.IsLetter(textBox1.Text, 0))
+            {
+                using (CataloghiEntities listaCataloghi = new CataloghiEntities())
+                {
+                    textBox1.Text = textBox1.Text.ToUpper();
+                    string header = textBox1.Text.Substring(0, 1);
+                    int number;
+                    int.TryParse(textBox1.Text.Substring(1, 3), out number);
+
+                    var cataQuery1 = from Cataloghi in listaCataloghi.Cataloghis where Cataloghi.PrefissoNome == header && Cataloghi.SuffissoNome == number orderby Cataloghi.SuffissoNome select Cataloghi;
+
+                    //cataQuery1.ToList();
+                    Console.WriteLine("Count: " + cataQuery1.ToList().Count);
+                    if (cataQuery1.ToList().Count > 0)
+                    {
+                        //ho almeno 1 record quindi il catalogo inserito esiste
+                        foreach (var catal in cataQuery1)
+                        {
+                            if (catal.DataChiusura==null && catal.IdUtenteChiusura==null)
+                            {
+                                pictureBox1.Image = AfterSalesDBEditor.Properties.Resources.checked_checkbox_64;
+                            }
+                            else
+                            {
+                                pictureBox1.Image = AfterSalesDBEditor.Properties.Resources.x_mark_5_64;
+                                MessageBox.Show("ERRORE: il catalogo inserito E'CHIUSO! Controllare inserimento!", "ERRORE", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                                textBox1.Focus();
+                            }
+                        }
+
+                        //MessageBox.Show("EUREKA!", "EUREKA", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        pictureBox1.Image = AfterSalesDBEditor.Properties.Resources.checked_checkbox_64;
+
+                    }
+                    else
+                    {
+                        pictureBox1.Image = AfterSalesDBEditor.Properties.Resources.x_mark_5_64;
+                        MessageBox.Show("ERRORE: il catalogo inserito non esiste! Controllare inserimento!", "ERRORE", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        textBox1.Focus();
+                    }
+
+
+                    listaCataloghi.Dispose();
+                }
+            }
+            else
+            {
+                pictureBox1.Image = AfterSalesDBEditor.Properties.Resources.x_mark_5_64;
+                MessageBox.Show("ATTENZIONE!! Dati inseriti non coerenti con quanto richiesto.\r\nControllare l'inserimento!", "ATTENZIONE!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                textBox1.Focus();
+            }
+
+
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            //    //lasciando il controllo verifico che l'inserito sia coerente. se si abilito pulsante
+            //    if (textBox2.Text.Length == 4 && char.IsLetter(textBox2.Text, 0))
+            //    {
+            //        textBox2.Text = textBox2.Text.ToUpper();
+
+            //        if (char.IsLetter(textBox2.Text, 0) && char.IsNumber(textBox2.Text, 1) && char.IsNumber(textBox2.Text, 2) && char.IsNumber(textBox2.Text, 3))
+            //        {
+
+
+            //        }
+            //        else
+            //        {
+
+            //        }
+            //    }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //controllo quando cambia il testo e quando è conforme abilito il pulsante
+            if (textBox2.Text.Length == 4 && char.IsLetter(textBox2.Text, 0) && char.IsNumber(textBox2.Text, 1) && char.IsNumber(textBox2.Text, 2) && char.IsNumber(textBox2.Text, 3))
+            {
+                pictureBox2.Image = AfterSalesDBEditor.Properties.Resources.checked_checkbox_64;
+                textBox2.Text = textBox2.Text.ToUpper();
+                button1.Visible = true;
+                button1.Enabled = true;
+            }
+            else
+            {
+                pictureBox2.Image = AfterSalesDBEditor.Properties.Resources.x_mark_5_64;
+                button1.Visible = false;
+                button1.Enabled = false;
+
+            }
+        }
+    }
+}
